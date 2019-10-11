cd88ffe93d672130fcb54a4eb465a18b8cad7d1e
Ruan Aragao <aragao.ruan@gmail.com>
2019-10-11 15:09:28 -0300

    [Add] Estilizando Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,102 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Home() {
   return (
-    <div>
-      <h1>HOME</h1>
-    </div>
+    <Container>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-campo-nike-mercurial-superfly-7-club-fg/58/HZM-1880-058/HZM-1880-058_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-campo-nike-mercurial-superfly-7-club-fg/58/HZM-1880-058/HZM-1880-058_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-campo-nike-mercurial-superfly-7-club-fg/58/HZM-1880-058/HZM-1880-058_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-campo-nike-mercurial-superfly-7-club-fg/58/HZM-1880-058/HZM-1880-058_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-campo-nike-mercurial-superfly-7-club-fg/58/HZM-1880-058/HZM-1880-058_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/chuteira-campo-nike-mercurial-superfly-7-club-fg/58/HZM-1880-058/HZM-1880-058_detalhe2.jpg?ims=326x"
+          alt="Tenis"
+        />
+        <strong>Tenis muito legal</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </Container>
   );
 }
